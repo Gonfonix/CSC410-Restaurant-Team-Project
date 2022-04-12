c3a239c820e71459161373d5c02e7f297e67416e
AJ Hammond <60402150+ajm4n@users.noreply.github.com>
2022-04-12 10:42:05 -0400

    added admin/not admin cookie track

--- LoginPage.aspx.cs
+++ LoginPage.aspx.cs
@@ -23,28 +23,38 @@ namespace CSC_410_Team_Project_Restaurant
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            //if (txtUserName.Text.Length > 0 && txtPassword.Text.Length > 0)
-            //{
-            //    dView = (DataView)sdsLogin.Select(DataSourceSelectArguments.Empty);
-            //    if (dView.Count > 0)
-            //    {
-            //        // successful login
-            //        dRowView = dView[0];
-            //        userType = (string)dRowView["UserType"];
-            //        userName = (string)dRowView["UserName"];
-            //        Session["UserType"] = userType;
-            //        Session["UserName"] = userName;
-            //        Response.Redirect("MainMenu.aspx");
-            //    }
-            //    else
-            //    {
-            //        lblStatus.Text = "Invalid User name or password";
-            //    }
-            //}
-            //else
-            //{
-            //    lblStatus.Text = "Please enter a User name and a Password";
-            //}
+            if (txtUserName.Text.Length > 0 && txtPassword.Text.Length > 0)
+            {
+                dView = (DataView)sdsLogin.Select(DataSourceSelectArguments.Empty);
+                if (dView.Count > 0)
+                {
+                    // successful login
+                    dRowView = dView[0];
+                    userType = (string)dRowView["UserType"];
+                    userName = (string)dRowView["UserName"];
+                    Session["UserType"] = userType;
+                    Session["UserName"] = userName;
+
+                    //if cookie = admin, go admin page, if not, go regular page
+                    if(Session["UserName"] == 'Administrator')
+                    {
+                        Response.Redirect("AdminPage.aspx");
+
+                    }
+                    else
+                    {
+                        Response.Redirect("MenuPage.aspx");
+                    }
+                }
+                else
+                {
+                    lblStatus.Text = "Invalid User name or password";
+                }
+            }
+            else
+            {
+                lblStatus.Text = "Please enter a User name and a Password";
+            }
         }
     }
 }
