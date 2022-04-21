720160b82d1dccd1f25894251fdaf7a610b2ca8d
AJ Hammond <60402150+ajm4n@users.noreply.github.com>
2022-04-21 10:10:54 -0400

    added forgotpassword
    need to edit the sql command to make it work

---
+++ ForgotPassword.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ForgotPassword.aspx.cs" Inherits="CSC_410_Team_Project_Restaurant.ForgotPassword" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 722px">
+    <form id="form1" runat="server">
+        <div>
+            Forgot Password?<br />
+            <br />
+            <asp:TextBox ID="txtUsername" runat="server" style="margin-bottom: 0px"></asp:TextBox>
+            <asp:Label ID="Label2" runat="server" Text="Username"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtPassword" runat="server" style="margin-bottom: 0px"></asp:TextBox>
+            <asp:Label ID="Label1" runat="server" Text="New Password"></asp:Label>
+        </div>
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+    </form>
+</body>
+</html>
---
+++ ForgotPassword.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CSC_410_Team_Project_Restaurant
+{
+    public partial class ForgotPassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string getConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["RestaurantConnectionString"].ConnectionString;
+        }
+        protected System.Void btnSubmit_Click(System.Object sender, System.EventArgs e)
+        {
+
+            if (txtUserName.Text.Length > 0 && txtPassword.Text.Length > 0)
+            {
+                sdsLogin.SelectCommand = "SELECT CustomerID, UserName, Password FROM Customer WHERE UserName = '" + txtUserName.Text + "' COLLATE SQL_Latin1_General_CP1_CS_AS AND Password = '" + txtPassword.Text + "' COLLATE SQL_Latin1_General_CP1_CS_AS UNION SELECT CAST(EmployeeID AS varchar), UserName, Password FROM Employee WHERE UserName = '" + txtUserName.Text + "' COLLATE SQL_Latin1_General_CP1_CS_AS AND Password = '" + txtPassword.Text + "' COLLATE SQL_Latin1_General_CP1_CS_AS";
+                //sdsLogin.DataBind();
+                dView = (DataView)sdsLogin.Select(DataSourceSelectArguments.Empty);
+                if (dView.Count > 0)
+                {
+                    dRowView = dView[0];
+
+                    userName = (string)dRowView["UserName"];
+
+                    password = (string)dRowView["Password"];
+
+                    if (int.TryParse((string)dRowView["CustomerID"], out int value))
+                    {
+                        userType = "Employee";
+                    }
+                    else
+                    {
+                        userType = "Customer";
+                    }
+
+                }
+            }
+            else
+            {
+                lblStatus.Text = "Please enter both a User Name and a Password.";
+            }
+
+
+        }
+    }
+}
